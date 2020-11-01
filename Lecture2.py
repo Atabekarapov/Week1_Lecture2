af84ff207b48107c3086bbf958ef0a608f23155e
Atabek <atabekarapov14@gmail.com>
2020-11-01 16:58:23 +0600

    Second commit

---
+++ Lecture2.py
@@ -0,0 +1,57 @@
+(# # name = "Atabek"
+# # surname = "Arapov"
+# # full_name = name + " " + surname
+# # print(full_name)
+
+
+# name = input("Enter your name: ")
+# # surname = input("Enter your surname: ")
+# # full_name = name + " " + surname
+# # print(full_name)
+
+# times = int(input("How many times you wanna print your name: "))
+# result = times * name
+# # print(result)
+
+
+
+# word = "Abcdefghigklmnop"
+# print(word[-3])
+# print(word[2])
+
+# word[x:y:z]          ASCII/Unicode
+# print(word[-4:4:-2])
+
+# word = "Atabek\nprogrammer"
+# print(word)
+
+# word = "\172"
+# print(word)
+
+# word = "atabek\\programmer"
+# print(word)
+
+# word = '''atabek     PEP8 72codes(it should be on one line)
+# mentor'''   (for long texts and url (that understand other people))
+# print(word)
+
+# String, int, float, bool, tuple - immutable(Ne izmenyayemoy)
+# Set is mutable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
